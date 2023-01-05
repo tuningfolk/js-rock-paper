dcee6386d929acd7d4557abe029ee9061fe75081
tuningfolk <alisahadges@gmail.com>
2023-01-05 09:52:26 +0530

    First trial

--- play.js
+++ play.js
@@ -1,3 +1,8 @@
+var i = 0;
+var player = 0;
+var computer = 0;
+var computerSelection;
+
 function getComputerChoice(){
         let x = Math.floor( Math.random()* 12);
         if(x<4){
@@ -10,28 +15,41 @@ function getComputerChoice(){
         }
 }
 
-
-
-function playRound(playerSelection, getComputerChoice){
+function playRound(playerSelection){
+    i++;
+    computerSelection = getComputerChoice();
 
     if(playerSelection.toUpperCase() == "ROCK"){
-        if(getComputerChoice.toUpperCase() == "SCISSORS"){
+        if(computerSelection.toUpperCase() == "SCISSORS"){
             return "player"
         }else{
             return "computer"
         }
     }else if(playerSelection.toUpperCase() == "PAPER"){
-        if(getComputerChoice.toUpperCase() == "ROCK"){
+        if(computerSelection.toUpperCase() == "ROCK"){
             return "player"
         }else{
             return "computer"
         }
     }else{
-        if(getComputerChoice.toUpperCase() == "PAPER"){
+        if(computerSelection.toUpperCase() == "PAPER"){
             return "player"
         }else{
             return "computer"
         }
     }
-    
+
+
 }
+
+function check(winner){
+    if(winner =='player'){
+        player++;
+    }else{
+        computer++;
+    }
+    document.getElementById("comp").innerHTML = computerSelection;
+    document.getElementById("winner").innerHTML = winner;
+    document.getElementById("num_of_rounds").innerHTML = player+computer;
+}
+
